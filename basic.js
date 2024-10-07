723735702d4be001ab99e0dd7ca6de95d5b7e883
yeeun0118s <yeeun0118s@naver.com>
2024-10-07 22:57:15 +0900

    원시적 타입(숫자, 문자열, 논리적)

--- basic.js
+++ basic.js
@@ -1,1 +1,24 @@
 console.log("Hello World");
+
+// 숫자형 데이터 타입
+Number =  1;
+console.log(Number);
+
+Number = 2;
+console.log(Number);
+
+Number = 1 + 2;
+console.log(Number);
+
+// String 데이터 타입
+String = "I am";
+
+let myName = "SunKyu";
+
+console.log(String + " " + myName);
+
+// Boolean 데이터 타입
+const a = true;
+const b = false;
+
+console.log(a && b); //&&은 관계연산자로 둘다 true일때 true를 반환
