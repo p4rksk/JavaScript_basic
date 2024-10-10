56036e6779825b13e01f0c45cf296a6061bbd676
yeeun0118s <yeeun0118s@naver.com>
2024-10-10 20:36:15 +0900

    symbol

--- basic.js
+++ basic.js
@@ -22,3 +22,9 @@ const a = true;
 const b = false;
 
 console.log(a && b); //&&은 관계연산자로 둘다 true일때 true를 반환
+
+//Symbol 데이터 타입
+const sym1 = Symbol("sunkyu");
+const sym2 = Symbol("sunkyu");
+
+console.log(sym1 === sym2); //false 각각의 symbol로 생성되기 때문에 다른 값을 가진다.
