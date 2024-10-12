f74eef8eea9a8896c4e95908da1aa5d7cc2ca0f3
yeeun0118s <yeeun0118s@naver.com>
2024-10-12 22:58:10 +0900

    조건문

--- basic.js
+++ basic.js
@@ -1,7 +1,9 @@
 // prompt: 사용자의 값을 입력받을 수 있는 창을 띄우는 함수
-let titleOfWork =parseInt(prompt("What work did you do?"));
+let titleOfWork =prompt("What work did you do?");
 
-//console.log(titleOfWork); 
-
-//isNaN(): 숫자 입력시 boolean을 반환 하는 함수
-console.log(isNaN(titleOfWork));
+//조건문
+if(titleOfWork == typeof(String)) {
+    console.log("작품명을 말씀 해주셔서 감사합니다.")
+} else {
+    console.log("작품명을 기입 해주세요")
+}
